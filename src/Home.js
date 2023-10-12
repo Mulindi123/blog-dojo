ab098375daf57343c61e23bf3c03dec51dec1220
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-12 22:56:14 +0300

    fetches data from json API

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,46 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1},
-        {title: "Welcome party!", body: "lorem ipsum...", author: "Yoshi", id: 2},
-        {title: "Web dev top tips", body: "lorem ipsum...", author: "Mario", id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+       
+
+
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
-        console.log("blog deleted")
+        // console.log("blog deleted")
     }
+
+    useEffect(() =>{
+        fetch(" http://localhost:8000/blogs")
+        .then(r => {
+            if(!r.ok){
+                throw Error("could not fetch data for that resource!")
+            }    
+            return r.json()})
+        .then(data=>{ 
+            setBlogs(data)
+            setIsLoading(false)
+            setError(null)
+        })
+        .catch(error=>{
+            setIsLoading(false)
+            setError(error.message)
+        })
+      
+    },[]);
+
 
     return ( 
         <div className="home">
-        <BlogList blogs ={blogs} title="All Blogs!" onDelete={handleDelete}/>
+        {error && <div>{error}</div>}
+        {isLoading && <div>Loading...</div>}
+        {blogs && <BlogList blogs ={blogs} title="All Blogs!" onDelete={handleDelete}/> }
         </div>
      );
 }
